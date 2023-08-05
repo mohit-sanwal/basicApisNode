a6ee87474715725391fc986905e50d1f4235b30c
mohitSanwal <mohit123ongraph@gmail.com>
2023-08-05 14:18:41 +0530

    loading static html pages via express.static func while adding html extention in the url

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+const publicPath = path.join(__dirname, 'public')
 
 
+// to statically accessing html pages while hitting in the browser url
+app.use(express.static(`${publicPath}`));
+
+// app.get('', (req, res) => {
+//    const filePath = 
+//    res.send(filePath);
+// })
 
 app.listen(5000);
