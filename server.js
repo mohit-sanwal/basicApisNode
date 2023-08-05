f3f475e556780872c201eea8a07bc8f05cb10532
mohitSanwal <mohit123ongraph@gmail.com>
2023-08-05 18:08:41 +0530

    adding middleware to group of routes

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const requestFilter = require('./middleware');
 const app = express();
 
+const route = express.Router();
+
 // applying middleware to single route/ the route we want
 // app.use(requestFilter);
-
-app.get('/', requestFilter, (req, res) => {
+route.use(requestFilter);
+route.get('/', (req, res) => {
     res.send('welcome to home page')
 }) 
 
-app.get('/users', (req, res) => {
+route.get('/users', (req, res) => {
     res.send('welcome to users page')
 }) 
 
@@ -18,6 +20,6 @@ app.get('/about', (req, res) => {
     res.send('welcome to about page')
 }) 
 
-
+app.use('/', route);
 
 app.listen(3000);
