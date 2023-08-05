cdccf18f133b0855c6c3d799a69f81bb4a68aba2
mohitSanwal <mohit123ongraph@gmail.com>
2023-08-05 14:25:06 +0530

    loading html pages without adding extention .html in url

--- server.js
+++ server.js
@@ -6,11 +6,14 @@ const publicPath = path.join(__dirname, 'public')
 
 
 // to statically accessing html pages while hitting in the browser url
-app.use(express.static(`${publicPath}`));
+// app.use(express.static(`${publicPath}`));
 
-// app.get('', (req, res) => {
-//    const filePath = 
-//    res.send(filePath);
-// })
+app.get('', (req, res) => {
+   res.sendFile(`${publicPath}/index.html`);
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(`${publicPath}/about.html`);
+})
 
 app.listen(5000);
