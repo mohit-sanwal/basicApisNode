dd29d4f5f531fff8c3eb98d5353e6643ce72c421
mohitSanwal <mohit123ongraph@gmail.com>
2023-08-05 14:50:55 +0530

    ejs templating engine

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const publicPath = path.join(__dirname, 'public')
+const publicPath = path.join(__dirname, 'public');
+
+app.set('view engine', 'ejs')
 
 
 // to statically accessing html pages while hitting in the browser url
 // app.use(express.static(`${publicPath}`));
+
+/* removing extention .html from the url */
 
 app.get('', (req, res) => {
    res.sendFile(`${publicPath}/index.html`);
@@ -15,5 +19,19 @@ app.get('', (req, res) => {
 app.get('/about', (req, res) => {
     res.sendFile(`${publicPath}/about.html`);
 })
+
+/* templating engine ejs */
+
+
+app.get('/profile', (req, res) => {
+    const user = {
+        name: 'mohit',
+        email: 'mohit123ongraph@gmail.com',
+        hobbies: ['sports', 'music']
+    }
+    res.render('profile', {user});
+})
+
+
 
 app.listen(5000);
