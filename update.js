eeb9fe6f27bfc50dee3fa9c2c485bb2e939ce90c
mohitSanwal <mohit123ongraph@gmail.com>
2023-08-06 09:24:14 +0530

    updated data into the db

---
+++ update.js
@@ -0,0 +1,18 @@
+const dbConnect = require('./mongodb');
+
+const updateData = async() => {
+    console.log('update called...')
+   const db = await dbConnect();
+   const result = await db.updateMany({name: "note 7"}, {$set: {name: "note pro 7", price: 1620}});
+
+   if (result.acknowledged) {
+      console.log('data updated', result);
+   }
+}
+
+
+updateData();
+
+
+// updateOne
+// updateMany
