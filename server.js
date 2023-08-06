df1ddaa9886ee2e6dae7f8cab21e09bd07aafe0d
mohitSanwal <mohit123ongraph@gmail.com>
2023-08-06 18:20:44 +0530

    read, delete, update api with mongoose

--- server.js
+++ server.js
@@ -19,6 +19,23 @@ app.get('/getProducts', async (_, resp) => {
     resp.send(data);
 })
 
+app.delete('/deleteProduct/:_id', async (req, resp) => {
+    const id = (req.params._id).trim();
+    console.log('req.params', req.params) // {_id: '34593252303nf893rw'}
+    // const data = await Product.deleteOne({_id: new mongodb.ObjectId(id)}); // when id with ObjectId('y8343y3249')
+    const data = await Product.deleteOne(req.params);
+    resp.send(data);
+})
+
+
+app.put('/updateProduct/:_id', async (req, resp) => {
+    const id = (req.params._id).trim();
+    // const data = await Product.updateOne({_id: new mongodb.ObjectId(id)}, {$set: req.body});
+    const data = await Product.updateOne(req.params, {$set: req.body});
+    console.log('data===', data);
+    resp.send(data);
+})
+
 
 
 
