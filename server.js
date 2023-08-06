c2f3508ff029083f74b8be6eb364c3c5ef5f2d9c
mohitSanwal <mohit123ongraph@gmail.com>
2023-08-06 18:07:59 +0530

    get api with mongoose

--- server.js
+++ server.js
@@ -2,15 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const mongodb = require('mongodb');
 require('./configs');
-const product = require('./product');
+const Product = require('./product');
 const app = express();
 
 app.use(express.json());
 
 app.post('/create', async (req, resp) => {
-    const data = new product(req.body);
+    const data = new Product(req.body);
     const result = await data.save();
     resp.send(result);
 })
+
+
+app.get('/getProducts', async (_, resp) => {
+    const data = await Product.find();
+    resp.send(data);
+})
+
+
+
 
 app.listen(5000);
