008c24e4a6a9ae5c9ec997a44cb63d5185ba324a
mohitSanwal <mohit123ongraph@gmail.com>
2023-08-06 09:23:46 +0530

    insert data into the db

---
+++ insert.js
@@ -0,0 +1,31 @@
+const dbConnect = require('./mongodb');
+
+const insert = async() => {
+    console.log('insert called...')
+   const db = await dbConnect();
+   const result = await db.insertMany([{
+    name: 'note 5',
+    brand: 'vivo',
+    price: 320,
+    category: 'mobile'
+   },
+   {
+    name: 'note 6',
+    brand: 'vivo',
+    price: 420,
+    category: 'mobile'
+   },
+   {
+    name: 'note 7',
+    brand: 'vivo',
+    price: 520,
+    category: 'mobile'
+   }]);
+
+   if (result.acknowledged) {
+      console.log('data inserted', result);
+   }
+}
+
+
+insert();
