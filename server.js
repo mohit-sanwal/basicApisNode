3eee11f2503fbfed7ac30a3bdf50edccf04e1a28
mohitSanwal <mohit123ongraph@gmail.com>
2023-08-06 08:10:33 +0530

    creating seprate file for dbConnection

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
-const {MongoClient} = require('mongodb');
+const dbConnect = require('./mongodb');
 
-const url = 'mongodb://localhost:27017';
-const database = 'e-comm';
-
-// to get the data from mongo data base
-const client = new MongoClient(url);
-
-async function getData() {
- let result = await client.connect();
- let db = result.db(database);
- let collection = db.collection('products');
- const response = await collection.find({}).toArray();
- console.log('data retrieved--', response);
+const main = async () => {
+  let data = await dbConnect();
+  let resData = await data.find().toArray();
+  console.warn(resData);
 }
 
+main();
 
-getData();
