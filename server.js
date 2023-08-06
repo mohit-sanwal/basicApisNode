6815ee48ecb8094769ed1b854f8581d1f98c68eb
mohitSanwal <mohit123ongraph@gmail.com>
2023-08-06 12:51:15 +0530

    using mongoose to use schemas and validation

---
+++ server.js
@@ -0,0 +1,47 @@
+
+const mongoose = require('mongoose');
+
+
+const main = async () => {
+    await mongoose.connect("mongodb://localhost:27017/e-comm");
+    const ProductSchema = new mongoose.Schema({
+        name:String,
+        price: Number
+    })
+    const ProductsModel = mongoose.model('products', ProductSchema);
+    let data = new ProductsModel({name: 'm8', price: 123});
+    let result = await data.save();
+    console.log(result);
+}
+
+main();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * 
+ * differce between mongo and mongoose, mongoose help us to prevent entering extra field
+ * 
+ * difference between schema and model
+ * schema - number of fields defined in database 
+ * model - it uses the schema to connect mongodb and node
+ * 
+ */
